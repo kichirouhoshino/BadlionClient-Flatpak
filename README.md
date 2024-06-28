86da7df15bf3c1f281641e6e2baa276bd917d876
rodlanbernabe <rodlanisagani@gmail.com>
2024-06-28 16:39:42 +0800

    Add more persist directories. Fixes login info

--- README.md
+++ README.md
@@ -4,9 +4,5 @@ A Flatpak definition of Badlion Client, a Minecraft launcher
 Based on the Lunar Client Flatpak manifest, since both launchers are packaged as an AppImage and are Electron apps
 https://github.com/flathub/com.lunarclient.LunarClient
 
-ToDo:
-- Add Flathub Stuff (tho idk if I can put it in flathub.)
-- Fix microsoft login not persisting after exit
-
 What I can't do:
 - zypak-wrapper (launching minecraft crashes the whole launcher. There's probably a way to do this but I definitely cant.)
--- apply_extra.sh
+++ apply_extra.sh
@@ -16,20 +16,20 @@ DEST="/app/extra/bin/"
 mkdir $DEST
 cp -r squashfs-root/* $DEST
 
-# Install icons
-
-ICON_DIR="/app/extra/export/share/icons/hicolor/"
-
-mkdir -p $ICON_DIR
-cp -r squashfs-root/usr/share/icons/hicolor/* $ICON_DIR
-
-iconSizes=("16" "32" "48" "64" "128" "256")
-
-for I in "${iconSizes[@]}"
-do
-	dir="$ICON_DIR/${I}x${I}/apps/"
-	mv "$dir/BadlionClient.png" "$dir/com.badlion.BadlionClient.png"
-done
+# Install icons. BadlionClient doesn't need this I believe
+#
+#ICON_DIR="/app/extra/export/share/icons/hicolor/"
+#
+#mkdir -p $ICON_DIR
+#cp -r squashfs-root/usr/share/icons/hicolor/* $ICON_DIR
+#
+#iconSizes=("16" "32" "48" "64" "128" "256")
+#
+#for I in "${iconSizes[@]}"
+#do
+#	dir="$ICON_DIR/${I}x${I}/apps/"
+#	mv "$dir/BadlionClient.png" "$dir/com.badlion.BadlionClient.png"
+#done
 
 # Clean up
 rm -rf squashfs-root/
