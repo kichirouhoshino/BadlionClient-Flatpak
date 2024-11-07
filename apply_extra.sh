6854cb7fa64222464665520f147719dd985e45be
Rodlan Bernabe <rodlanisagani@gmail.com>
2024-11-07 08:00:58 +0800

    move script to yaml

--- apply_extra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mv ./BadlionClient ./BadlionClient.appimage
-chmod +x ./BadlionClient.appimage
-unappimage ./BadlionClient.appimage
-mkdir /app/badlionclient
-cp -r squashfs-root/* /app/badlionclient
-rm -rf squashfs-root/
-rm ./BadlionClient.appimage
